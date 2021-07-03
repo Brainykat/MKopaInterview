d05236ad87ba02f08ebfbb07cce7de0dfcc69991
Chris <eng.christhuku@gmail.com>
2021-07-03 09:41:05 +0300

    feat building block initial classes

---
+++ src/BuildingBlocks/SharedBase/Common.Base.Shared/EntityBase.cs
@@ -0,0 +1,59 @@
+﻿using StackifyLib.Utils;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Common.Base.Shared
+{
+  public class EntityBase
+  {
+    [Key]
+    public virtual Guid Id { get; set; }
+    public bool IsTransient() => Id == Guid.Empty;
+
+    /// <summary>
+    /// See https://github.com/stackify/stackify-api-dotnet/blob/master/Src/StackifyLib/Utils/SequentialGuid.cs
+    /// </summary>
+    /// <param name="type"></param>
+    public void GenerateNewIdentity()
+    {
+      if (IsTransient())
+      {
+        Id = SequentialGuid.NewGuid();
+      }
+    }
+
+    public void ChangeCurrentIdentity(Guid id)
+    {
+      if (id != Guid.Empty)
+        Id = id;
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (!(obj is EntityBase other))
+        return false;
+      if (ReferenceEquals(this, other))
+        return true;
+      if (GetType() != other.GetType())
+        return false;
+      if (other.IsTransient() || IsTransient())
+        return false;
+      return Id == other.Id;
+    }
+
+    public static bool operator ==(EntityBase left, EntityBase right)
+    {
+      if (left is null && right is null)
+        return true;
+      if (left is null || right is null)
+        return false;
+      return left.Equals(right);
+    }
+
+    public static bool operator !=(EntityBase left, EntityBase right) => !(left == right);
+
+    public override int GetHashCode() => (GetType().ToString() + Id).GetHashCode();
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    
+  }
+}
---
+++ src/BuildingBlocks/SharedBase/Common.Base.Shared/ValueObjects/Money.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Base.Shared.ValueObjects
+{
+  public class Money
+  {
+    private Money() { }
+    private Money(string currency, decimal amount, DateTime time)
+    {
+      if (string.IsNullOrWhiteSpace(currency)) throw new ArgumentNullException(nameof(currency));
+      if (amount < 0M) throw new ArgumentOutOfRangeException(nameof(amount));
+      Currency = currency.Trim().ToUpper(); Amount = amount; Time = time;
+    }
+    public static Money Create(string currency, decimal amount, DateTime? time = null) =>
+        new Money(currency, amount, time != null ? time.Value : DateTime.UtcNow);
+    
+    public string Currency { get;  } // The setters are for elastic De-serialize only
+    public decimal Amount { get;  }
+    public DateTime Time { get;  }
+    public override string ToString() => $"{Currency} {Amount:N2}";
+    public override bool Equals(object obj)
+    {
+      return obj is Money money &&
+             Currency == money.Currency &&
+             Amount == money.Amount &&
+             Time == money.Time;
+    }
+    public override int GetHashCode()
+    {
+      var hashCode = 624119691;
+      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Currency);
+      hashCode = hashCode * -1521134295 + Amount.GetHashCode();
+      hashCode = hashCode * -1521134295 + Time.GetHashCode();
+      return hashCode;
+    }
+    public static bool operator ==(Money left, Money right)
+    {
+      return EqualityComparer<Money>.Default.Equals(left, right);
+    }
+    public static bool operator !=(Money left, Money right)
+    {
+      return !(left == right);
+    }
+  }
+}
